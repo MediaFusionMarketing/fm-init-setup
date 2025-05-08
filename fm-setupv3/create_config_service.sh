d1038e2d1c98d34b452952e8c383b07c3356688d
nxyzo <134438059+nxyzo@users.noreply.github.com>
2025-05-08 15:39:39 +0200

    Update create_config_service.sh

--- fm-setupv3/create_config_service.sh
+++ fm-setupv3/create_config_service.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# 1) Skript aus /root ins systemweite bin-Verzeichnis kopieren
 install -m0755 /root/install.sh /usr/local/bin/install.sh
 
-# 2) systemd-Unit anlegen
 cat > /etc/systemd/system/install.service <<'EOF'
 [Unit]
 Description=Run initial configuration at next boot
@@ -13,8 +11,8 @@ Wants=network-online.target
 
 [Service]
 Type=oneshot
+RemainAfterExit=yes
 ExecStart=/usr/local/bin/install.sh
-# Nach erfolgreichem Lauf den Dienst deaktivieren und systemd neu laden
 ExecStartPost=/usr/bin/systemctl disable install.service
 ExecStartPost=/usr/bin/systemctl daemon-reload
 
@@ -22,5 +20,6 @@ ExecStartPost=/usr/bin/systemctl daemon-reload
 WantedBy=multi-user.target
 EOF
 
-# 3) Service aktivieren
+# Neu laden, aktivieren
+systemctl daemon-reload
 systemctl enable install.service
